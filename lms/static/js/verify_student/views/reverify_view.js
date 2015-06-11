f414945616ca381e628e3a1adfb2529741f53654
Will Daly <will@edx.org>
2015-06-11 08:56:30 -0400

    More WIP

--- lms/static/js/verify_student/views/reverify_view.js
+++ lms/static/js/verify_student/views/reverify_view.js
@@ -12,7 +12,8 @@
     edx.verify_student.ReverifyView = Backbone.View.extend({
         el: '#reverify-container',
 
-        subviews: [],
+        stepOrder: ["face-photo-step", "id-photo-step", "review-photos-step", "reverify-success-step"],
+        stepViews: {},
 
         initialize: function( obj ) {
             this.errorModel = obj.errorModel || null;
@@ -21,67 +22,44 @@
         },
 
         initializeStepViews: function( stepInfo ) {
-            // var i,
-            //     stepName,
-            //     stepData,
-            //     subview,
-            //     subviewConfig,
-            //     nextStepTitle,
-            //     subviewConstructors,
-            //     verificationModel;
+            var verificationModel, errorModel, stepViewConstructors, nextStepTitles;
 
             // We need to initialize this here, because
             // outside of this method the subview classes
             // might not yet have been loaded.
-            subviewConstructors = {
+            stepViewConstructors = {
                 'face-photo-step': edx.verify_student.FacePhotoStepView,
                 'id-photo-step': edx.verify_student.IDPhotoStepView,
                 'review-photos-step': edx.verify_student.ReviewPhotosStepView,
                 'reverify-success-step': edx.verify_student.ReverifySuccessStepView
             };
 
+            nextStepTitles = [
+                gettext( "Take photo" ),
+                gettext( "Take a photo of your ID" ),
+                gettext( "Review your info" ),
+                gettext( "Submit" )
+            ];
+
             // Create the verification model, which is shared
             // among the different steps.  This allows
             // one step to save photos and another step
             // to submit them.
-            var verificationModel = new edx.verify_student.VerificationModel(),
-                errorModel = this.errorModel;
+            verificationModel = new edx.verify_student.VerificationModel();
+            errorModel = this.errorModel;
 
-            _.each(["face-photo-step", "id-photo-step", "review-photos-step", "reverify-success-step"], function(name) {
-                var subviewConfig = {
-                    errorModel: this.error
-                }
+            _.each(this.stepOrder, function(name) {
+                var stepView = new stepViewConstructors[name]({
+                    errorModel: errorModel,
+                    nextStepTitle: nextStepTitles[name],
+                    stepData: stepInfo[name]
+                });
 
+                this.listenTo(stepView, 'next-step', this.nextStep);
+                this.listenTo(stepView, 'go-to-step', this.goToStep);
+
+                this.stepViews[name] = stepView;
             });
-
-            this.subviews.push(edx.verify_student.FacePhotoStepView(stepInfo["face-photo-step"]));
-            this.subviews.push(edx.verify_student.IDPhotoStepView(stepInfo["id-photo-step"]));
-            this.subviews.push(edx.verify_student.ReviewPhotosStepView(stepInfo["review-photos-step"]));
-            this.subviews.push(edx.verify_student.ReverifySuccessStepView(stepInfo["reverify-success-step"]));
-
-                    subviewConfig = {
-                        errorModel: this.errorModel,
-                        templateName: this.displaySteps[i].templateName,
-                        nextStepTitle: nextStepTitle,
-                        stepData: stepData
-                    };
-
-                    // For photo verification steps, set the shared photo model
-                    if ( this.VERIFICATION_VIEW_NAMES.indexOf(stepName) >= 0 ) {
-                        _.extend( subviewConfig, { model: verificationModel } );
-                    }
-
-                    // Create the subview instance
-                    // Note that we are NOT yet rendering the view,
-                    // so this doesn't trigger GET requests or modify
-                    // the DOM.
-                    this.subviews[stepName] = new subviewConstructors[stepName]( subviewConfig );
-
-                    // Listen for events to change the current step
-                    this.listenTo( this.subviews[stepName], 'next-step', this.nextStep );
-                    this.listenTo( this.subviews[stepName], 'go-to-step', this.goToStep );
-                }
-            }
         },
 
         render: function() {
@@ -90,7 +68,7 @@
         },
 
         renderCurrentStep: function() {
-            var stepName, stepView, stepEl;
+            var stepView, stepEl;
 
             // Get or create the step container
             stepEl = $("#current-step-container");
@@ -98,13 +76,9 @@
                 stepEl = $('<div id="current-step-container"></div>').appendTo(this.el);
             }
 
-            // Render the subview
-            // Note that this will trigger a GET request for the
-            // underscore template.
-            // When the view is rendered, it will overwrite the existing
-            // step in the DOM.
-            stepName = this.displaySteps[ this.currentStepIndex ].name;
-            stepView = this.subviews[ stepName ];
+            // Render the step subview
+            // When the view is rendered, it will overwrite the existing step in the DOM.
+            stepView = this.stepViews[ this.stepOrder[ this.currentStepIndex ] ];
             stepView.el = stepEl;
             stepView.render();
         },
@@ -112,16 +86,13 @@
         nextStep: function() {
             this.currentStepIndex = Math.min(
                 this.currentStepIndex + 1,
-                this.displaySteps.length - 1
+                this.stepOrder.length - 1
             );
             this.render();
         },
 
         goToStep: function( stepName ) {
-            var stepIndex = _.indexOf(
-                _.pluck( this.displaySteps, 'name' ),
-                stepName
-            );
+            var stepIndex = _.indexOf(this.stepOrder, stepName);
 
             if ( stepIndex >= 0 ) {
                 this.currentStepIndex = stepIndex;
